e4dab830c4aa76ceb5264834b20f3f5eb6771ae9
= <=>
2024-01-14 13:44:26 -0700

    add firebase

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		hmr: {
+			protocol: 'ws',
+			port: 5443
+		}
 	}
 });
