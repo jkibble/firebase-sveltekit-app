22cd092930964c4b24d2bb717e75603f9dffa4d1
= <=>
2024-02-01 20:27:47 -0700

    remove unused functions directory. update firebase libs, add dictionary,json for future game use. Update typing game with more features

--- src/routes/game4/+page.svelte
+++ src/routes/game4/+page.svelte
@@ -1,49 +1,107 @@
 <script lang="ts">
+  import { faker } from "@faker-js/faker/locale/en";
   import { onMount } from "svelte";
+  import wordList from "$lib/word_list.json";
 
-  const randomWikipediaArticle = async () => {
-    const response = await fetch("https://en.wikipedia.org/api/rest_v1/page/random/summary");
-    return await response.json();
-  };
-
-  let article: any = {};
   let correctLetters: string[] = [];
   let letterIndex = 0;
-  let typedLetters: { letter: string; correct: null | true | false }[] = [];
+  let typedLetters: { letter: string; correct: null | true | false; incorrect?: string }[] = [];
+  let wordsPerMinute = 0;
+  let numberOfWords = 0;
+  let errors = 0;
+  let timer = 0;
+  const commonWords = ["the", "of", "is", "and", "in", "with", "a", "to", "for", "on"];
 
-  const onKeyDown = (event: KeyboardEvent) => {
+  const startTimer = () => {
+    const startTime = new Date().getTime();
+    wordsPerMinute = 0;
+    const interval = setInterval(() => {
+      timer++;
+      const currentTime = new Date().getTime();
+      const elapsedTime = (currentTime - startTime) / 1000 / 60;
+      wordsPerMinute = Math.round(numberOfWords / elapsedTime);
+    }, 1000);
+  };
+
+  const onKeyDown = async (event: KeyboardEvent) => {
     if (event.metaKey || event.ctrlKey || event.altKey) return;
+    const typedKey = event.key;
 
-    if (event.key === "Backspace") {
-      typedLetters.pop();
+    if (typedKey === "Backspace") {
+      const removed = typedLetters.pop();
+      if (removed?.letter === " ") numberOfWords--;
+      if (removed?.correct === false) errors--;
       letterIndex = Math.max(0, letterIndex - 1);
       typedLetters = [...typedLetters];
     }
 
-    if (event.key === "Enter") updateArticle();
-
-    if (event.key.length === 1) {
-      if (event.key === correctLetters[letterIndex]) {
-        typedLetters.push({ letter: event.key, correct: true });
-      } else {
-        typedLetters.push({ letter: event.key, correct: false });
-      }
-
-      letterIndex = Math.min(correctLetters.length, letterIndex + 1);
-      event.preventDefault();
-      typedLetters = [...typedLetters];
+    if (typedKey === "Enter") {
+      typedLetters = [];
+      correctLetters = await makeParagraph();
     }
+
+    if (typedKey.length !== 1) return;
+    if (typedKey === " " && correctLetters[letterIndex] !== " ") return;
+
+    if (typedKey === " ") {
+      numberOfWords++;
+    }
+
+    event.preventDefault();
+
+    if (letterIndex === 0) {
+      startTimer();
+    }
+
+    if (typedKey === correctLetters[letterIndex]) {
+      typedLetters.push({ letter: typedKey, correct: true });
+    } else {
+      typedLetters.push({
+        letter: typedKey,
+        correct: false,
+      });
+      errors++;
+    }
+
+    letterIndex = Math.min(correctLetters.length, letterIndex + 1);
+    typedLetters = [...typedLetters];
   };
 
-  const updateArticle = async () => {
-    article = await randomWikipediaArticle();
-    correctLetters = article.extract.split("");
-    typedLetters = [];
+  const makeParagraph = async () => {
+    const words = faker.helpers.arrayElements(wordList, 500);
+    const sentenceCount = faker.number.int({ min: 4, max: 6 }); // Random sentence count (between 4 and 6)
+    let paragraph = "";
     letterIndex = 0;
+    wordsPerMinute = 0;
+    numberOfWords = 0;
+    errors = 0;
+
+    for (let i = 0; i < sentenceCount; i++) {
+      const sentenceLength = faker.number.int({ min: 5, max: 10 }); // Random sentence length (between 5 and 10 words)
+      let sentence = "";
+
+      for (let j = 0; j < sentenceLength; j++) {
+        if (Math.random() < 0.2 && j < sentenceLength - 1) {
+          const randomCommonIndex = faker.number.int({ min: 0, max: commonWords.length });
+          sentence += " " + commonWords[randomCommonIndex];
+        } else {
+          const randomIndex = faker.number.int({ min: 0, max: words.length });
+          sentence += " " + words[randomIndex];
+        }
+      }
+
+      // Capitalize the first word of the sentence
+      sentence = sentence.trim();
+      sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1);
+
+      paragraph += sentence + ". ";
+    }
+
+    return paragraph.split("");
   };
 
   onMount(async () => {
-    await updateArticle();
+    correctLetters = await makeParagraph();
   });
 </script>
 
@@ -51,26 +109,23 @@
 
 <h3>Game 4</h3>
 
-<div class="m-auto max-w-[900px] rounded-xl border border-black bg-gray-600 p-6 font-mono text-2xl">
-  <p>
+<div class="flex-colr flex justify-between p-5">
+  <span>WPM: {wordsPerMinute}</span>
+  <span>Number of Words: {numberOfWords}</span>
+  <span>Errors: {errors}</span>
+  <span>Timer: {Math.floor(timer / 60)}:{timer % 60}</span>
+</div>
+
+<div class="m-auto whitespace-pre-wrap rounded-xl border p-5 text-2xl text-gray-600">
+  <p class="font-mono leading-loose">
     {#each correctLetters as letter, i}
       {#if typedLetters[i]?.correct}
-        <span class="correct">{letter}</span>
+        <span class="text-white">{letter}</span>
       {:else if typedLetters[i]?.correct === false}
-        <span class="wrong">{typedLetters[i].letter}</span>
+        <span class="text-red-500">{typedLetters[i].letter}</span>
       {:else}
         <span>{letter}</span>
       {/if}
     {/each}
   </p>
 </div>
-
-<style>
-  .correct {
-    color: greenyellow;
-  }
-
-  .wrong {
-    color: red;
-  }
-</style>
